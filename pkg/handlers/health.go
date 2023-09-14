7c20d40dc1f371b0110b5aa3a23f5ae67d7aafeb
Dylan Ross <pdylanross@gmail.com>
2023-09-14 18:11:44 -0500

    feat: added helm charts index

--- pkg/handlers/health.go
+++
@@ -1,9 +0,0 @@
-package handlers
-
-import "github.com/gin-gonic/gin"
-
-func setupHealthHandlers(router *gin.Engine) {
-	router.GET("/livez", func(context *gin.Context) {
-		context.Status(200)
-	})
-}
